add ComplexExmp Op that performs comples exponential with symbolic derivative. Fix severe bug when Unitary.complex_dot always returns zero

manifolds/unitary.py
@@ -29,7 +29,7 @@ from theano import tensor as T, Op, Apply
 
 from theano.tensor import slinalg, as_tensor_variable
 
-class Expm(Op):
+class ComplexExpm(Op):
     """
     Compute the matrix exponential of a square array.
     """
@@ -51,10 +51,89 @@ class Expm(Op):
         temp = scipy.linalg.expm(A[0, :, :] + 1j * A[1, :, :])
         expm[0] = np.stack([temp.real, temp.imag])
 
+    def grad(self, inputs, outputs):
+        (A,) = inputs
+        (g_out,) = outputs
+        return [ComplexExpmGrad()(A, g_out)]
+
     def infer_shape(self, node, shapes):
         return [shapes[0]]
 
-expm = Expm()
+
+def _hconj_internal(x):
+    x_hconj = np.transpose(x, axes=(0, 2, 1)).copy()
+    x_hconj[1, :, :] = -x_hconj[1, :, :]
+    return x_hconj
+
+
+class ComplexExpmGrad(Op):
+    """
+    Gradient of the matrix exponential of a square array.
+    """
+
+    __props__ = ()
+
+    def make_node(self, A, gw):
+        assert imported_scipy, (
+            "Scipy not available. Scipy is needed for the Expm op")
+        A = as_tensor_variable(A)
+        assert A.ndim == 3
+        out = theano.tensor.tensor3(dtype=A.dtype)
+        return Apply(self, [A, gw], [out, ])
+
+    def infer_shape(self, node, shapes):
+        return [shapes[0]]
+
+    def perform(self, node, inputs, outputs):
+        # Kalbfleisch and Lawless, J. Am. Stat. Assoc. 80 (1985) Equation 3.4
+        # Kind of... You need to do some algebra from there to arrive at
+        # this expression.
+
+        (A, gA) = inputs
+        (out,) = outputs
+
+        w, V = scipy.linalg.eig(A[0, :, :] + 1j * A[1, :, :], right=True)
+        U = scipy.linalg.inv(V)
+
+        exp_w = np.exp(w)
+        X = np.subtract.outer(exp_w, exp_w) / np.subtract.outer(w, w)
+        np.fill_diagonal(X, exp_w)
+        Y = np.conj(V.dot(U.dot(gA[0, :, :].T - 1j * gA[1, :, :].T).dot(V) * X).dot(U)).T
+
+        out[0] = np.stack([Y.real, Y.imag]).astype(A.dtype)
+
+        """
+        w, V = scipy.linalg.eig(A[0, :, :] + 1j * A[1, :, :], right=True)
+        V = V.T
+        #print(np.sum(w.imag), np.sum(V.imag))
+        U = scipy.linalg.inv(V)
+
+        exp_w = np.exp(w)
+        X = np.subtract.outer(exp_w, exp_w) / np.subtract.outer(w, w)
+        np.fill_diagonal(X, exp_w)
+        #print(X.real, X.imag)
+        real_middle = V.dot(gA[0, ...]).dot(U)
+        imag_middle = V.dot(gA[1, ...]).dot(U)
+        middle = V.dot(gA[0, :, :] - 1j * gA[1, :, :]).dot(U)
+        #middle = middle.real * X.real + 1j * middle.imag * X.imag
+        middle = middle * X
+        Y = U.dot(middle).dot(np.conj(V).T)
+
+        out[0] = np.stack([Y.real, -Y.imag]).astype(A.dtype)
+        """
+        """
+        n = A.shape[1]
+        fun = hconj
+        Z = np.zeros_like(A)
+        scale = la.norm(gA)
+        gA /= scale
+        res_mat = np.concatenate([np.concatenate([A, fun(gA)], axis=2),
+                                  np.concatenate([Z, A], axis=2)], axis=1)
+        res = scipy.linalg.expm(res_mat[0, :, :] + 1j * res_mat[1, :, :])
+        out_mat = np.stack([res[:n, n:].real, res[:n, n:].imag], axis=0)
+        out[0] = fun(out_mat * scale)
+        """
+complex_expm = ComplexExpm()
 
 
 class UnitaryKron(Manifold):
@@ -219,7 +298,7 @@ class Unitary(Manifold):
             self._name = 'Complex Stiefel manifold St({}, {})'.format(n, p)
         else:
             self._name = 'Product complex Stiefel manifold St({}, {})^{}'.format(n, p, k)
-        self._exponential = True
+        self._exponential = False
 
 
     @property
@@ -237,13 +316,21 @@ class Unitary(Manifold):
     def frac(self, A):
         return A[0, :, :], A[1, :, :]
 
+    def complex_dot_(self, A, B):
+        A_real, A_imag = self.frac(A)
+        B_real, B_imag = self.frac(B)
+        re = A_real.dot(B_real) - A_imag.dot(B_imag)
+        im = A_real.dot(B_imag) + A_imag.dot(B_real)
+
+        return T.stack([re, im], axis=0)
+
     def complex_dot(self, A, B):
         A_real, A_imag = self.frac(A)
         B_real, B_imag = self.frac(B)
-        dotted = T.zeros((2, A_real.shape[0], B_real.shape[1]))
-        T.set_subtensor(dotted[0, :, :], A_real.dot(B_real) - A_imag.dot(B_imag))
-        T.set_subtensor(dotted[1, :, :], A_real.dot(B_imag) + A_imag.dot(B_real))
-        return dotted
+        prod = T.zeros((2, A_real.shape[0], B_real.shape[1]))
+        prod = T.set_subtensor(prod[0, :, :], A_real.dot(B_real) - A_imag.dot(B_imag))
+        prod = T.set_subtensor(prod[1, :, :], A_real.dot(B_imag) + A_imag.dot(B_real))
+        return prod
 
     def transpose(self, X):
         return T.transpose(X, axes=(0, 2, 1))
@@ -310,18 +397,19 @@ class Unitary(Manifold):
         Y = T.stack([Q.real, Q.imag])
         return Y
 
-    def stack(self, arrays, axis):
+    def concat(self, arrays, axis):
         return T.concatenate(arrays, axis=axis+1)
 
     def exp(self, X, U):
         # The exponential (in the sense of Lie group theory) of a tangent
         # vector U at X.
-        first = self.stack([X, U], axis=1)
+        first = self.concat([X, U], axis=1)
         XhU = self.complex_dot(self.hconj(X), U)
-        second = expm(self.stack([self.stack([XhU, -self.complex_dot(self.hconj(U), U)], 1),
-                                  self.stack([self.identity(), XhU], 1)], 0))
-        third = self.stack([expm(-XhU), self.zeros()], 0)
-        return self.complex_dot(self.complex_dot(first, second), third)
+        second = complex_expm(self.concat([self.concat([XhU, -self.complex_dot(self.hconj(U), U)], 1),
+                                  self.concat([self.identity(), XhU], 1)], 0))
+        third = self.concat([complex_expm(-XhU), self.zeros()], 0)
+        exponential = self.complex_dot(self.complex_dot(first, second), third)
+        return exponential
 
     def log(self, X, Y):
         # The logarithm (in the sense of Lie group theory) of Y. This is the
@@ -368,3 +456,30 @@ class Unitary(Manifold):
             raise ValueError('FixedRankEmbeeded.lincomb takes 3 or 5 arguments')
 
 
+class DoubleComplex(Op):
+    """
+    Compute the matrix exponential of a square array.
+    """
+
+    __props__ = ()
+
+    def make_node(self, A):
+        assert imported_scipy, (
+            "Scipy not available. Scipy is needed for the Expm op")
+
+        A = as_tensor_variable(A)
+        assert A.ndim == 3
+        sumc = theano.tensor.tensor3(dtype=A.dtype)
+        return Apply(self, [A, ], [sumc, ])
+
+    def perform(self, node, inputs, outputs):
+        (A,) = inputs
+        (sumc,) = outputs
+        sumc[0] = 2 * A
+
+    def grad(self, inputs, outputs):
+        (A,) = inputs
+        (g_out,) = outputs
+        return [g_out * 2]
+
+dcom = DoubleComplex()